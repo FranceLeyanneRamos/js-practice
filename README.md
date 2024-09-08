bb82b79473d65ab2a34ab18d2ac187e8dc11c746
FranceLeyanneRamosramosfranceleyanne@gmail.com <you@example.com>
2024-09-08 16:05:52 +0800

    Completed Variables Section

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# js-practice
-js-practice
+<!--  -->
---
+++ app.js
@@ -0,0 +1,36 @@
+console.log("Hello World!");
+let name = ("FRANCE LEYANNE P. RAMOS");
+console.log('My name is' + name);
+
+console.log("Variables");
+let x = 10;
+let y = 20;
+const z = 30;
+console.log("x, y, z");
+
+console.log("Numbers");
+let num1 = 9;
+let num2 = 14;
+let quotient = num1 / num2;
+console.log( quotient);
+
+console.log("Booleans");
+let age = 25;
+if (age >= 18) {
+    console.log("You are an adult.");
+    } else {
+    console.log("You are a minor"); }
+
+console.log("Comparison Operators");
+let x = 9;
+let y = 14;
+
+if (x === y) {
+    console.log(x, "and", y, "is equal"); 
+} else {
+    console.log(x, "and", y, "is not equal"); 
+}
+
+console.log("Strings");
+let message = 'My name is {France Leyanne Ramos}.';
+console.log(message);
